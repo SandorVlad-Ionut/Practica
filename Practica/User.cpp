86fe43c80ea41f8f2660a79309f3f066bc0e15a0
SandorVlad <61762962+SandorVlad@users.noreply.github.com>
2021-12-19 19:41:16 +0200

    Add project files.

---
+++ Practica/User.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include<list>
+#include"User.h"
+using namespace std;
+
+User::User(string firstname, string surname, int age, char goals)
+{
+	idGenerator++;
+	this->m_id = idGenerator;
+	this->m_surname = surname;
+	this->m_firstname = firstname;
+	this->m_goals = goals;
+	this->m_age = age;
+}
+
+
+int User::getId() const
+{
+	return this->m_id;
+}
+
+
+void User::setFirstname(string name)
+{
+	this->m_firstname = name;
+}
+
+string User::getFirstname() const
+{
+	return this->m_firstname;
+}
+
+
+void User::setSurname(string name)
+{
+	this->m_surname = name;
+}
+
+string User::getSurname() const
+{
+	return this->m_surname;
+
+}
+
+
+
+	void User::setAge(int age)
+	{
+		this->m_age = age;
+	}
+
+	int User::getAge() const
+	{
+		return this->m_age;
+	}
+
+
+
+
+
---
+++ Practica/User.h
@@ -0,0 +1,37 @@
+#pragma once
+using namespace std;
+#include <string>
+
+class User
+{
+public:
+	User();
+	User(string firstname,string surname,int age,char goals);
+
+	void setFirstname(string name);
+	string getFirstname() const;
+
+	void setSurname(string name);
+	string getSurname() const;
+
+	void setAge(int age);
+	int getAge() const;
+
+	## sa fac un modul separat pt goals sau sa il pun aici?
+	## goals sa fie categorii prestabilite de ex "sport", "istorie" sau stringuri facute de user : "sa fac cozonaci cu nuca mai bine" ?
+
+
+	int getId() const;
+
+
+protected:
+	string m_firstname;
+	string m_surname;
+	char m_goals;
+	int m_age;;
+
+private:
+	int m_id;
+	static int idGenerator;
+
+};
